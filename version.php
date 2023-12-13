c032377764b68a0c07a502aa548a16b8b7db6ade
hatemaskar <hatemaskar@gmail.com>
2023-12-13 16:31:23 +0200

    update

--- version.php
+++ version.php
@@ -3,7 +3,9 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_addpage'; // Full name of the plugin.
 $plugin->version   = 2022040101;      // The current plugin version.
-$plugin->requires  = 2018051700;      // Requires this Moodle version.
+$plugin->requires  = 2018051700;      
+$plugin->maturity = MATURITY_STABLE; 
+$plugin->release = '1.0 (Build: 2023010100)'; 
 
 // Author details (not standard, for informational purposes only)
 $plugin->author    = 'UMAMI E-Learning Solutions';
